f7f0ca341728fafa789a3972f031e9123ace5bd4
WahyuSattriana <wahyusattriana@gmail.com>
2025-07-01 21:51:24 +0800

    chore: seed to Users and fix unique room name

---
+++ seeders/20250701134654-seed-to-User.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { hashPass } = require('../migrations/helpers/bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    let data = require('../db/Users.json');
+    data = data.map((e) => {
+      e.password = hashPass(e.password);
+      e.createdAt = new Date();
+      e.updatedAt = new Date();
+      return e;
+    });
+
+    await queryInterface.bulkInsert('Users', data);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
